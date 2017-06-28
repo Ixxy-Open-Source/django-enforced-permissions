3ddc9012e6a729b100cd8b311b02be7c3fabe2b1
Chen Zhe <fruitschen@gmail.com>
2017-06-28 16:08:13 +0800

    bug fix: post_migrate is not emitted for applications that lack a models module

---
+++ enforced_permissions/models.py
@@ -0,0 +1,3 @@
+# django.db.models.signals.post_migrate
+# Sent at the end of the migrate (even if no migrations are run) and flush commands.
+# It’s not emitted for applications that lack a models module.
